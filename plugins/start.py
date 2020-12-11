8a9232142b5f8f1ec488ebfbad18b8ff51c1a5cc
Jihudumie <61816001+Jihudumie@users.noreply.github.com>
2020-12-11 15:47:36 +0300

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -12,3 +12,12 @@ async def start(client, message):
     welcomed = f"Hey <b>{message.from_user.first_name}</b>\n/help for More info"
     await message.reply_text(welcomed, reply_markup=joinButton)
     raise StopPropagation
+
+@Client.on_message(Filters.command(["juhudi"]), group=-2)
+async def start(client, message):
+    # return
+    joinButton = InlineKeyboardMarkup([
+        [InlineKeyboardButton("Quran", url="https://t.me/Furqanbot")],
+        [InlineKeyboardButton(
+            "Viongozi", url="https://t.me/ViongiziBot")]
+    ])
