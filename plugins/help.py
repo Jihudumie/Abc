4f1cae298c4a98f55d6638b15949b865d6193aab
Jihudumie <61816001+Jihudumie@users.noreply.github.com>
2020-12-12 09:19:34 +0300

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -5,3 +5,8 @@ from pyrogram import Client, Filters
 async def start(client, message):
     helptxt = f"https://telegra.ph/I-LOVE-ISLAM-04-21"
     await message.reply_text(helptxt)
+
+@Client.on_message(Filters.command(["islam"]))
+async def islamu(client, message):
+    helptxt = f"https://telegra.ph/I-LOVE-ISLAM-04-21"
+    await message.reply_text(helptxt)
