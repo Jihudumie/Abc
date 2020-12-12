2ed00641d410e55a79f672cc98e36a4ffa480194
Jihudumie <61816001+Jihudumie@users.noreply.github.com>
2020-12-12 17:18:50 +0300

    Create abd.py

---
+++ plugins/abd.py
@@ -0,0 +1,14 @@
+from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, InlineKeyboardMarkup
+
+
+@Client.on_message(Filters.command(["hamis"]), group=-2)
+async def start(client, message):
+    # return
+    gehabutton = InlineKeyboardMarkup([
+        [InlineKeyboardButton("KITABU", url="https://t.me/Furqanbot")],
+        [InlineKeyboardButton(
+            "NYUMBANI", url="https://t.me/Abdallaahbot")]
+    ])
+    abdallaah = f"Hey <b>{message.from_user.first_name}</b>\n/Master Karibu Saaana"
+    await message.reply_text(abdallaah, reply_markup=gehabutton)
+    raise StopPropagation
