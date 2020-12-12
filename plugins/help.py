9bc1a9f9debe02d0f40ca84ec9c619e677b204c2
Jihudumie <61816001+Jihudumie@users.noreply.github.com>
2020-12-12 17:03:06 +0300

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -8,10 +8,6 @@ async def start(client, message):
 
 @Client.on_message(Filters.command(["juzuu"]))
 async def islamu(client, message):
-    tugeme = f"kitabu \n  https://t.me/Furqanbot"
-    juzuutxt = f"""https://telegra.ph/Juzuu-ya-1-Hadi-Ya-30-01-22
-
-
-https://t.me/Furqanbot
-"""
-    await message.reply_text(juzuutxt, tugeme)
+    tugeme_juzu = f"kitabu \n  https://t.me/Furqanbot"
+    juzuutxt = f"https://telegra.ph/Juzuu-ya-1-Hadi-Ya-30-01-22"
+    await message.reply_text(juzuutxt, tugeme_juzu)
