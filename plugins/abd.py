78121d56deaadb3fd62fea24c3d357e97e4171b4
Jihudumie <61816001+Jihudumie@users.noreply.github.com>
2020-12-12 17:31:30 +0300

    Update abd.py

--- plugins/abd.py
+++ plugins/abd.py
@@ -5,8 +5,8 @@ from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, Inl
 async def start(client, message):
     # return
     gehabutton = InlineKeyboardMarkup([
-        [InlineKeyboardButton("KITABU", url="https://t.me/Furqanbot")],
         [inlineKeyboardButton("JINA", url="https://telegra.ph/017---Al-Israa-12-29")],
+        [InlineKeyboardButton("KITABU", url="https://t.me/Furqanbot")],
         [InlineKeyboardButton(
             "NYUMBANI", url="https://t.me/Abdallaahbot")]
     ])
