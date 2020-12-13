be4091ed80a07a93ea672f09b70f9b6adb129ba5
Jihudumie <61816001+Jihudumie@users.noreply.github.com>
2020-12-13 07:04:41 +0300

    Update juhud.py

--- plugins/juhud.py
+++ plugins/juhud.py
@@ -6,7 +6,7 @@ from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, Inl
 async def start(client, message):
     # return
     juhudbutton = InlineKeyboardMarkup([
-        [InlineKeyboardButton("Maana Ya Jihaad Na Kuwekewa Shari'ah", url="https://telegra.ph/Jihaad-Maana-Ya-Jihaad-Na-Kuwekewa-Shariah-01-24")],
+        [InlineKeyboardButton("Maana Ya Jihaad", url="https://telegra.ph/Jihaad-Maana-Ya-Jihaad-Na-Kuwekewa-Shariah-01-24")],
         [InlineKeyboardButton("Khawaarij", url="https://telegra.ph/Khawaarij-04-16")],
         [InlineKeyboardButton("Jihaad Ni Waajib", url="https://telegra.ph/Jihaad-Ni-Waajib-01-24")],
         [InlineKeyboardButton("Wajibu Wa Kuwa Thabiti", url="https://telegra.ph/Wajibu-Wa-Kuwa-Thabiti-01-24")],
@@ -14,8 +14,10 @@ async def start(client, message):
         [InlineKeyboardButton("ajibu Wa Viongozi Wa Jeshi", url="https://telegra.ph/Wajibu-Wa-Viongozi-Wa-Jeshi-01-24")],
         [InlineKeyboardButton("Al-Wahn", url="https://telegra.ph/Al-Wahn-01-24")],
         [InlineKeyboardButton("ISLAMIC STATE", url="https://telegra.ph/Ribbiyyuwna-04-26")], 
+        [InlineKeyboardButton("📚 KUJITOA 👇 MUHANGA 📖", url="https://telegra.ph/KUJITOA-MUHANGA-02-21")],
+        [InlineKeyboardButton("1. A'MALIYAT ISTISHAHADIYAH", url="https://telegra.ph/AMALIYAT-ISTISHAHADIYAH--KUJITOA-MUHANGA--KWA-MUJIBU-WA-QURAN--SUNNAH--NA-KAULI-MBALI-MBALI-ZA-WANACHUONI-02-21")],
         [InlineKeyboardButton(
-            "📚 KUJITOA MUHANGA 📖", url="https://telegra.ph/KUJITOA-MUHANGA-02-21")]
+            "2. SHARTI ZA KUJITOA MUHANGA", url="https://telegra.ph/UHALI-WA-KUJITOA-MUHANGA-KWA-MUJIBU-WA-QURAN-02-21")]
         
     ])
 
