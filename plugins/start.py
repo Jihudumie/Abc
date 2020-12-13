5642c8c5809776f4e49cf6362e68b871a5c0d0a2
Jihudumie <61816001+Jihudumie@users.noreply.github.com>
2020-12-13 07:18:51 +0300

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -1,14 +1,31 @@
-from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram import Client, Filters
 
 
-@Client.on_message(Filters.command(["start"]), group=-2)
+@Client.on_message(Filters.command(["start", "start@JihaadBot"]))
 async def start(client, message):
-    # return
-    joinButton = InlineKeyboardMarkup([
-        [InlineKeyboardButton("Quran", url="https://t.me/Furqanbot")],
-        [InlineKeyboardButton(
-            "Maktabah", url="https://t.me/Abdallaahbot")]
-    ])
-    welcomed = f"Hey <b>{message.from_user.first_name}</b>\n/help for More info"
-    await message.reply_text(welcomed, reply_markup=joinButton)
-    raise StopPropagation
+    welcomed = f"Hey <b>{message.from_user.first_name}</b>\n Karibu ktk Majaribio"
+    await message.reply_text(welcomed)
+
+@Client.on_message(Filters.command(["help", "help@JihaadBot"]))
+async def help(client, message):
+    help_txt = f"""<b>Hapa ni sehemu ya Msaada</b>
+
+🖲 <a
+href='https://telegra.ph/i-love-islam-01-24'>I LOVE ISLAM</a>
+
+Kama una Hitaji Kusoma Au Kusiliza Qur'an Tukufu.
+❖ @Furqanbot
+
+<u>Kwa Darsa Mbali Mbali. 
+Txt 📄, Audio 🎧, Video & File N.k.</u>
+❖ @Hamisbot
+
+Kuongea Nasi Au Viongozi Tuandikie ✍.
+👉 @ViongoziBot Na Shidayako itafika Kwetu
+Kwa Idhini ya Allah Tutakusaidia In Shaa Allah.
+
+Allah Akujaalie Wepesi Katika Mambo yako na Akupe <b>Mwisho Mwema</b> Aamiyn"""
+    await message.reply_text(help_txt)
+
+
+
