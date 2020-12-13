18d1278755a71d6aab190a346a53f31c48c37368
Jihudumie <61816001+Jihudumie@users.noreply.github.com>
2020-12-13 10:58:56 +0300

    Update buluwgh.py

--- plugins/buluwgh.py
+++ plugins/buluwgh.py
@@ -1,7 +1,7 @@
 from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, InlineKeyboardMarkup
 
 
-@Client.on_message(Filters.command(["buluwgh"]), group=-2)
+@Client.on_message(Filters.command(["buluwgh", "buluwgh@JihaadBot"]), group=-2)
 async def start(client, message):
     # return
     buluwghbutton = InlineKeyboardMarkup([
